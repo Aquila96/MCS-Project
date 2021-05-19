d11006c8d6b79dccb827052447946e4e009a4d78
Ewen Smith <ewens@student.unimelb.edu.au>
2021-05-19 23:54:48 +1000

    Refactored results wealth groups to classes

--- result.py
+++ result.py
@@ -7,9 +7,9 @@ class Result:
         self.tick = []
         self.wealth_min = []
         self.wealth_max = []
-        self.wealth_group_low = []
-        self.wealth_group_mid = []
-        self.wealth_group_upper = []
+        self.wealth_class_low = []
+        self.wealth_class_mid = []
+        self.wealth_class_upper = []
         self.gini_index = []
 
     def load_result(self, tick, world, agents):
@@ -21,7 +21,7 @@ class Result:
         self.wealth_min = world.wealth_min
         self.wealth_max = world.wealth_max
         self.__append_gini_index(world, agents)
-        self.__append_wealth_grouping(agents)
+        self.__append_wealth_classes(agents)
 
     def __append_gini_index(self, world, agents):
         """Calculates gini index and appends to results"""
@@ -31,19 +31,19 @@ class Result:
         linear_lorenz_step = world.wealth_max/len(agents)
         self.gini_index += [sum([linear_lorenz_step*i - sum(wealth_list[:i]) for i in wealth_list])]
 
-    def __append_wealth_grouping(self, agents):
-        low_group = 0
-        mid_group = 0
-        up_group = 0
+    def __append_wealth_classes(self, agents):
+        low_class = 0
+        mid_class = 0
+        up_class = 0
 
         for agent in agents:
             if agent.get_wealth <= self.wealth_max[-1]/3:
-                low_group += 1
+                low_class += 1
             elif agent.get_wealth <= self.wealth_max[-1]*2/3:
-                mid_group += 1
+                mid_class += 1
             else:
-                up_group += 1
+                up_class += 1
 
-        self.wealth_group_low += [low_group]
-        self.wealth_group_mid += [mid_group]
-        self.wealth_group_upper += [up_group]
+        self.wealth_class_low += [low_class]
+        self.wealth_class_mid += [mid_class]
+        self.wealth_class_upper += [up_class]
