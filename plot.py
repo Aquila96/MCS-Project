a3dc649d1561fe484669fbab23376d75767149ee
rakhitha <dor@student.unimelb.edu.au>
2021-05-24 23:11:58 +1000

    CSV output, average gini

--- plot.py
+++ plot.py
@@ -1,7 +1,21 @@
 import matplotlib.pyplot as plt
+import csv
 
 
 def plot_gini(tick, gini_index):
-    if (tick[-1] % 100) == 0:
+    if (tick[-1] % 200) == 0:
         plt.plot(tick, gini_index)
         plt.show()
+
+        gini_average = [average(gini_index)]
+        print(gini_average[-1])
+
+        with open('./gini-index_data.csv', 'w') as f:
+            # create the csv writer
+            writer = csv.writer(f)
+            # write a row to the csv file
+            writer.writerow(gini_average)
+            writer.writerow(gini_index)
+
+def average(lst):
+    return sum(lst) / len(lst)
