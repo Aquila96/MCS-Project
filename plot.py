24247cd0ed09407727b3a54b6c72de2b846e0d2e
rakhitha <dor@student.unimelb.edu.au>
2021-05-26 19:02:08 +1000

    CSV output variable results

--- plot.py
+++ plot.py
@@ -3,18 +3,21 @@ import csv
 
 
 def plot_gini(tick, gini_index):
-    if (tick[-1] % 200) == 0:
+    if (tick[-1] % 100) == 0:
+        # plot gini distribution
         plt.plot(tick, gini_index)
         plt.show()
+
+        # plot
 
         gini_average = [average(gini_index)]
         print(gini_average[-1])
 
-        with open('./gini-index_data.csv', 'w') as f:
+        with open('./gini-index_data.csv', 'a') as f:
             # create the csv writer
             writer = csv.writer(f)
             # write a row to the csv file
-            writer.writerow(gini_average)
+            # writer.writerow(gini_average)
             writer.writerow(gini_index)
 
 def average(lst):
